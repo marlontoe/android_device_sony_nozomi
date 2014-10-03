7cebd9bb9c76e9ec7ef86e01f6ffe83bb6e7520c
marlontoe <inclusionrec@googlemail.com>
2014-10-03 12:47:05 +0200

    CARBONROM: initial commit

--- carbon.mk
+++ carbon.mk
@@ -12,21 +12,21 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
- 
-# Torch
-PRODUCT_PACKAGES := \
-    Torch
 
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+# Inherit from nozomi
+$(call inherit-product, device/sony/nozomi/full_nozomi.mk)
 
-# Inherit from nozomi device
-$(call inherit-product, device/sony/nozomi/nozomi.mk)
+# Inherit CM common Phone stuff.
+$(call inherit-product, vendor/carbon/config/common_phone.mk)
 
-# Set those variables here to overwrite the inherited values.
-PRODUCT_NAME := full_nozomi
+# Boot Animation
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 720
+
+# Inherit CARBON common Phone stuff.
+$(call inherit-product, vendor/carbon/config/common_phone.mk)
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=LT26i_1257-5499 BUILD_FINGERPRINT=SEMC/LT26i_1257-5499/LT26i:4.0.4/6.1.A.2.50/zfd_zw:user/release-keys PRIVATE_BUILD_DESC="LT26i-user 4.0.4 6.1.A.2.50 zfd_zw test-keys"
+
+PRODUCT_NAME := carbon_nozomi
 PRODUCT_DEVICE := nozomi
-PRODUCT_BRAND := Sony
-PRODUCT_MANUFACTURER := Sony
-PRODUCT_MODEL := Xperia S
