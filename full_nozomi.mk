c8b49c8f6a8708f4def4521ca638fe52a62faacc
marlontoe <inclusionrec@googlemail.com>
2015-01-06 15:50:06 +0100

    build omniswitch

--- full_nozomi.mk
+++ full_nozomi.mk
@@ -16,7 +16,8 @@
  
 # Torch
 PRODUCT_PACKAGES := \
-    Torch
+    Torch \
+    OmniSwitch
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
