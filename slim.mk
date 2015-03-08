19a6b06fb0f61f5ccd8afb58ee057e3adfdbadaa
marlontoe <inclusionrec@googlemail.com>
2015-03-08 09:55:24 +0100

    slim lp5.0 initial bring up

---
+++ slim.mk
@@ -0,0 +1,25 @@
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 720
+
+# Specify phone tech before including full_phone
+$(call inherit-product, vendor/slim/config/gsm.mk)
+
+# Inherit Slim common Phone stuff.
+$(call inherit-product, vendor/slim/config/common_full_phone.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/slim/config/nfc_enhanced.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/sony/nozomi/full_nozomi.mk)
+
+# Release name
+PRODUCT_RELEASE_NAME := nozomi
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := nozomi
+PRODUCT_NAME := slim_nozomi
+PRODUCT_BRAND := Xperia
+PRODUCT_MODEL := S
+PRODUCT_MANUFACTURER := Sony
